980ba45e7b7909efed8a425178842f4e6ef28aa6
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-05-01 12:49:44 +0400

    Add mvc models and make ajax call
    create a class inside serach module and there store result of making ajax call inside object search, then import it in controller model (index.js)

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
-import str from './models/Search';
-//import { add as a, multiply as m, ID } from './views/searchView';
-import * as searchView from './views/searchView';
+import Search from './models/Search';
 
-console.log(`Using imported function! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}.`);
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
+
+
+
+
 
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am an export string';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'http://cors-anywhere.herokuapp.com/'
+        const key = '62f578b82d5965f2931973e3821ac808'
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    };
+}
+
